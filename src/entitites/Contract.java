ae5aaa56c7a6f65b7912402c6458f4643c2fe259
Rian Sousa <riansousa666@gmail.com>
2024-09-11 10:11:33 -0300

    Classe Contract inicialmente feita

---
+++ src/entitites/Contract.java
@@ -0,0 +1,39 @@
+package entitites;
+
+import java.time.LocalDate;
+
+public class Contract {
+	private Integer number;
+	private LocalDate date;
+	private Double totalValue;
+	
+	public Contract(Integer number, LocalDate date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+	
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public Double getTotalValue() {
+		return totalValue;
+	}
+	
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+}
