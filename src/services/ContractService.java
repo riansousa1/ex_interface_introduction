6b931f5974c28d59681ad121a2a33bb0e24f3f69
Rian Sousa <riansousa666@gmail.com>
2024-09-16 10:47:16 -0300

    finalizado metodo processContract

--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -1,6 +1,9 @@
 package services;
 
+import java.time.LocalDate;
+
 import entitites.Contract;
+import entitites.Installment;
 
 public class ContractService {
 	
@@ -14,6 +17,19 @@ public class ContractService {
 	
 	public void processContract(Contract contract, int months) {
 		
+		// valor basico de quota
+		double basicQuota = contract.getTotalValue() / months;
+		
+		for (int i = 1; i <= months; i ++) {
+			LocalDate dueDate = contract.getDate().plusMonths(i); // encontra a data um mês depois
+			
+			double interest = onlinePaymentService.interest(basicQuota, i); // encontra a taxa basica baseada no mês
+			double fee = onlinePaymentService.paymentFee(basicQuota + interest); // encontra a taxa fixa
+			double totalQuota = basicQuota + interest + fee; // quota total
+			
+			contract.getInstallments().add(new Installment(dueDate, totalQuota)); // adiciona a nova data e valor na lista 
+		}
+
 	}
 	
 	
